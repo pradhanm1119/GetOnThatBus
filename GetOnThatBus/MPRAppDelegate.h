c598ce57a8a32a6ea0f898b354f899c16db773df
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-25 13:45:10 -0500

    Initial Commit

---
+++ GetOnThatBus/MPRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPRAppDelegate.h
+//  GetOnThatBus
+//
+//  Created by Manas Pradhan on 3/25/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GetOnThatBus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GetOnThatBus
+//
+//  Created by Manas Pradhan on 3/25/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MPRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPRAppDelegate class]));
+    }
+}
